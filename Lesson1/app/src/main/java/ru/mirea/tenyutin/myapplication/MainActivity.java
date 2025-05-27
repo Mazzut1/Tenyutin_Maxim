e599cb28ef3ff4c7662fc1e0577105454f17dc20
Eugenia <185727387+Mazzut1@users.noreply.github.com>
2025-05-27 17:38:58 +0300

    Lesson1

---
+++ Lesson1/app/src/main/java/ru/mirea/tenyutin/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package ru.mirea.tenyutin.myapplication;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.table_layout);
+        ViewCompat.setOnApplyWindowInsetsListener(
+                findViewById(R.id.main), (v, insets) -> {
+                    Insets bars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    v.setPadding(bars.left, bars.top, bars.right, bars.bottom);
+                    return insets;
+                });
+    }
+}
